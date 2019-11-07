04149bb3c5fa9b375eaa9232f11fcabe4a63d3a2
kinzlee <44349401+kinzlee@users.noreply.github.com>
2019-11-07 14:47:23 +0100

    Update index.js
    changed it from the old syntax in es5 to the new syntax for your variable declaration

--- server/plugins/index.js
+++ server/plugins/index.js
@@ -1,7 +1,7 @@
 module.exports = registerPlugins
 
-var log = require('npmlog')
-var requireResolve = require('./resolver')
+const log = require('npmlog')
+const requireResolve = require('./resolver')
 
 function checkModule (modulePath) {
   try {
@@ -16,11 +16,11 @@ function checkModule (modulePath) {
 }
 
 function registerPlugins (server, options, callback) {
-  var hapiPlugins = [
+  const hapiPlugins = [
     require('inert')
   ]
 
-  var localPlugins = [
+  const localPlugins = [
     './client',
     './logger',
     './maybe-force-gzip',
@@ -32,14 +32,14 @@ function registerPlugins (server, options, callback) {
     }
   })
 
-  var externalPlugins = options.plugins
+  const externalPlugins = options.plugins
     .filter(function (pluginPath) {
       return checkModule(pluginPath + '/hoodie/server')
     })
     .map(function (pluginPath) {
-      var pkg = require(pluginPath + '/package.json')
-      var pluginName = pkg.hoodie ? pkg.hoodie.name || pkg.name : pkg.name
-      var hapiPluginOptions = require(pluginPath + '/hoodie/server')
+      const pkg = require(pluginPath + '/package.json')
+      const pluginName = pkg.hoodie ? pkg.hoodie.name || pkg.name : pkg.name
+      const hapiPluginOptions = require(pluginPath + '/hoodie/server')
 
       if (!hapiPluginOptions.register) {
         hapiPluginOptions = {register: hapiPluginOptions}
@@ -51,7 +51,7 @@ function registerPlugins (server, options, callback) {
       return hapiPluginOptions
     })
 
-  var plugins = hapiPlugins.concat(localPlugins, externalPlugins)
+  const plugins = hapiPlugins.concat(localPlugins, externalPlugins)
 
   log.silly('hapi', 'Registering plugins')
 
